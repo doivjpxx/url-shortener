673186687d4e8744fc72f4be5d69d844d6e42879
phong.bnh <doivjpxx@gmail.com>
2024-09-02 16:33:48 +0700

    feat: add handler unit test

--- src/handlers/url_handlers.rs
+++ src/handlers/url_handlers.rs
@@ -97,3 +97,95 @@ pub async fn delete_url(
         )),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::db::connect_database;
+
+    #[tokio::test]
+    async fn test_create_url() {
+        dotenvy::dotenv().ok();
+
+        let pool = connect_database().await;
+        let app_state = Arc::new(AppState { pool });
+
+        let response = create_url(
+            State::from(axum::extract::State(Arc::clone(&app_state))),
+            Json(CreateUrlRequest {
+                url: "https://example.com".to_string(),
+                short_code: "example".to_string(),
+            }),
+        )
+        .await;
+
+        assert_eq!(response.is_ok(), true);
+    }
+
+    #[tokio::test]
+    async fn test_retrieve_url() {
+        dotenvy::dotenv().ok();
+
+        let pool = connect_database().await;
+        let app_state = Arc::new(AppState { pool });
+
+        let response = retrieve_url(
+            State::from(axum::extract::State(Arc::clone(&app_state))),
+            Path("example".to_string()),
+        )
+        .await;
+
+        assert_eq!(response.is_ok(), true);
+    }
+
+    #[tokio::test]
+    async fn test_update_url() {
+        dotenvy::dotenv().ok();
+
+        let pool = connect_database().await;
+        let app_state = Arc::new(AppState { pool });
+
+        let response = update_url(
+            State::from(axum::extract::State(Arc::clone(&app_state))),
+            Path("example".to_string()),
+            Json(UpdateUrlRequest {
+                url: "https://example.com".to_string(),
+            }),
+        )
+        .await;
+
+        assert_eq!(response.is_ok(), true);
+    }
+
+    #[tokio::test]
+    async fn test_get_stats_url() {
+        dotenvy::dotenv().ok();
+
+        let pool = connect_database().await;
+        let app_state = Arc::new(AppState { pool });
+
+        let response = get_stats_url(
+            State::from(axum::extract::State(Arc::clone(&app_state))),
+            Path("example".to_string()),
+        )
+        .await;
+
+        assert_eq!(response.is_ok(), true);
+    }
+
+    #[tokio::test]
+    async fn test_delete_url() {
+        dotenvy::dotenv().ok();
+
+        let pool = connect_database().await;
+        let app_state = Arc::new(AppState { pool });
+
+        let response = delete_url(
+            State::from(axum::extract::State(Arc::clone(&app_state))),
+            Path("example".to_string()),
+        )
+        .await;
+
+        assert_eq!(response.is_ok(), true);
+    }
+}
